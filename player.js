01c3be00f776d60e00853452e19609739a03ad36
Colin Jaffe <colin.jaffe@gmail.com>
2019-04-01 12:31:12 -0400

    refactors attack method

--- player.js
+++ player.js
@@ -3,18 +3,22 @@ const player = {
     maxAttack: 5,
     armor: 1,
     level: 1,
+
     attack: function(enemy) {
         this.hitPoints -= this.calculateAttackDamage(enemy.maxAttack);
         enemy.hitPoints -= this.calculateAttackDamage(this.maxAttack);
     },
+
     calculateAttackDamage: function(maxAttack) {
-        const randomEnemyAttack = randomizeAttack(maxAttack);
+        const randomAttack = randomizeAttack(maxAttack);
         const damageTaken = randomAttack - this.armor;
         return Math.max(damageTaken, 1);
     },
+
     randomizeAttack: function(attackMax) {
         return Math.floor(Math.random() * attackMax);
     },
+
     levelUp: function() {
         this.attackDamage = Math.floor(this.attackDamage * 1.2);
         this.level = this.level + 1;
