3f2b25dddbccf39f4878aa82394887471557ae60
Pierce Collazo <pierce.collazo@codeimmersive.com>
2019-04-03 15:05:31 -0400

    Filled in objects and playtested to ensure all is working. Accidentally managed to finish the healing stretch goal in the process, so that's cool.

--- monster.js
+++ monster.js
@@ -1,3 +1,11 @@
 const monster = {
-
+    isAlive: true,
+    hitPoints: 30,
+    lastDamageTaken: 0,
+    respawn: function (){
+        this.isAlive = true;
+        this.hitPoints = 30;
+        player.hitPoints += Math.ceil(Math.random() * 50);
+    }
 };
+
--- player.js
+++ player.js
@@ -1,3 +1,21 @@
 const player = {
+    isAlive: true,
+    hitPoints: 100,
+    level: 1,
+    lastDamageTaken: 0,
+    fight: function(monster){
+       this.lastDamageTaken = Math.ceil(Math.random() * 5);
+       monster.lastDamageTaken = Math.ceil(Math.random() * 5);
+       this.hitPoints = this.hitPoints - this.lastDamageTaken;
+       monster.hitPoints = monster.hitPoints - monster.lastDamageTaken;
 
+       if(monster.hitPoints < 1){
+           monster.isAlive = false;
+       } else if(this.hitPoints <= 0){
+           player.isAlive = false;
+       }
+    },
+    levelUp: function(){
+        this.level += 1;
+    },
 }
